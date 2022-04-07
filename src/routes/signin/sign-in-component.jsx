f619585443ccb008f65e119d4b180d1cc4171d40
goldmouthdogg88 <goldmouthdogg88@gmail.com>
2022-04-07 19:44:20 -0400

    Last minute changes

--- src/routes/signin/sign-in-component.jsx
+++ src/routes/signin/sign-in-component.jsx
@@ -1,28 +1,19 @@
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
-
 import {
-  auth,
   signinWithGooglePopup,
   signinWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
-  // useEffect(async () => {
-  //   const response = await getRedirectResult(auth);
-  //   console.log(response);
-  // }, []);
-
-  useEffect(() => {
-    async function getResponse() {
-      const response = await getRedirectResult(auth);
-      if (response) {
-        const userDocRef = await createUserDocumentFromAuth(response.user);
-      }
-    }
-    getResponse();
-  });
+  // useEffect(() => {
+  //   async function getResponse() {
+  //     const response = await getRedirectResult(auth);
+  //     if (response) {
+  //       const userDocRef = await createUserDocumentFromAuth(response.user);
+  //     }
+  //   }
+  //   getResponse();
+  // });
 
   const logGoogleUser = async () => {
     const { user } = await signinWithGooglePopup();
@@ -33,9 +24,6 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      <button onClick={signinWithGoogleRedirect}>
-        Sign in with Google Redirect
-      </button>
     </div>
   );
 };
