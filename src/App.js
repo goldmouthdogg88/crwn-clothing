49bc42fb9ea7b166870a680b6f7098c80c3983bf
goldmouthdogg88 <goldmouthdogg88@gmail.com>
2022-04-07 12:40:18 -0400

    Completed 80-react-router-outlet

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
-import "./categories.styles.scss";
-import CategoryItem from "./components/category-item/category-item.component";
-const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
+import { Routes, Route } from "react-router-dom";
 
+import "./categories.styles.scss";
+import Home from "./routes/home/home.component";
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
+
+const App = () => {
   return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
+    <Routes>
+      <Route path="/home" element={<Home />}>
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
   );
 };
 
