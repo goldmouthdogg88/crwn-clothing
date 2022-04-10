6b79d3bd6722090b1f799cd5f005b1bbcc7471b3
PTRN330 <ptrn330@iu.edu>
2022-04-10 13:24:45 -0400

    Completed 102-re-rendering-from-context

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,7 +6,8 @@ import "./navigation.styles.scss";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
-  console.log(currentUser);
+  // console.log(currentUser);
+
   return (
     <Fragment>
       <div className="navigation">
